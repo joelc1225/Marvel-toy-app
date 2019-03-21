8e1baf0e7aabedfae713cd9898a6a708515cd948
Joel Camargo <joelc1225@gmail.com>
2019-03-21 14:19:37 -0400

    Initial commit

---
+++ app/src/main/java/com/onramp/android/takehome/ui/CharacterDetailFragment.java
@@ -0,0 +1,32 @@
+package com.onramp.android.takehome.ui;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.onramp.android.takehome.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CharacterDetailFragment extends Fragment {
+
+
+    public CharacterDetailFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_blank_fragment);
+        return textView;
+    }
+
+}
---
+++ app/src/main/java/com/onramp/android/takehome/ui/CharacterOverviewActivity.java
@@ -0,0 +1,15 @@
+package com.onramp.android.takehome.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.onramp.android.takehome.R;
+
+public class CharacterOverviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_character_overview);
+    }
+}
