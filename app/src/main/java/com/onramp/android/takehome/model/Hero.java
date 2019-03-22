8d1c32c3a0e351b7d490a8a23183cf26c7653262
Joel Camargo <joelc1225@gmail.com>
2019-03-22 14:05:26 -0400

    Strip Hero class to bare minumum. Marvel JSON response was too complex

--- app/src/main/java/com/onramp/android/takehome/model/Hero.java
+++ app/src/main/java/com/onramp/android/takehome/model/Hero.java
@@ -3,20 +3,21 @@ package com.onramp.android.takehome.model;
 import org.parceler.Parcel;
 
 @Parcel
-public class Character {
+public class Hero {
 
-    String name;
-    String description;
-    String imagePath;
+    // Needs to public for Parceler library
+    public String name;
+    public String description;
+    public String imagePath;
 
 
-    public Character(String name, String description, String imagePath) {
+    public Hero(String name, String description, String imagePath) {
         this.name = name;
         this.description = description;
         this.imagePath = imagePath;
     }
 
     // Empty constructor needed for Parceler library
-    public Character() {
+    public Hero() {
     }
 }
