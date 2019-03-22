f8aaff1ac47d5ead25219c98053bf64a825ef0e2
Joel Camargo <joelc1225@gmail.com>
2019-03-22 14:06:03 -0400

    Finish main recyclerView

--- app/src/main/java/com/onramp/android/takehome/ui/MainRecyclerviewAdapter.java
+++ app/src/main/java/com/onramp/android/takehome/ui/MainRecyclerviewAdapter.java
@@ -1,4 +1,80 @@
 package com.onramp.android.takehome.ui;
 
-public class MainRecyclerviewAdapter {
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.onramp.android.takehome.R;
+import com.onramp.android.takehome.model.Hero;
+import com.squareup.picasso.Picasso;
+
+import org.parceler.Parcels;
+
+import java.util.List;
+
+import timber.log.Timber;
+
+public class MainRecyclerviewAdapter extends RecyclerView.Adapter<MainRecyclerviewAdapter.ViewHolder> {
+
+    private Context mContext;
+    private List<Hero> mHeroesList;
+
+
+    public MainRecyclerviewAdapter(Context context, List<Hero> heroesList) {
+        this.mContext = context;
+        this.mHeroesList = heroesList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View itemView = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.hero_list_item, viewGroup, false);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        final Hero currentHero = mHeroesList.get(i);
+
+        Picasso.get().load(currentHero.imagePath).centerCrop().fit().into(viewHolder.imageView);
+        Timber.d("image path is: %s", currentHero.imagePath);
+        viewHolder.nameTextview.setText(currentHero.name);
+
+        viewHolder.constraintLayout.setOnClickListener(v -> {
+            Intent goToOverviewIntent = new Intent(mContext, CharacterOverviewActivity.class);
+
+            goToOverviewIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            Bundle bundle = new Bundle();
+            bundle.putParcelable("hero", Parcels.wrap(currentHero));
+            goToOverviewIntent.putExtra("bundle", bundle);
+            mContext.startActivity(goToOverviewIntent);
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mHeroesList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView nameTextview;
+        ImageView imageView;
+        ConstraintLayout constraintLayout;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            nameTextview = itemView.findViewById(R.id.item_textView);
+            imageView = itemView.findViewById(R.id.item_imageView);
+            constraintLayout = itemView.findViewById(R.id.item_constraint_layout);
+        }
+    }
 }
