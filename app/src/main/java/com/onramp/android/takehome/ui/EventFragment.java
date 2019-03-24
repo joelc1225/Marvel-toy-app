3f0ec3332156a0870c13099ba32fb8d866e70179
Joel Camargo <joelc1225@gmail.com>
2019-03-24 01:03:24 -0400

    Event activity and EventFrag REALLY done. last commit was layout files -_-

--- app/src/main/java/com/onramp/android/takehome/ui/EventFragment.java
+++ app/src/main/java/com/onramp/android/takehome/ui/EventFragment.java
@@ -1,29 +1,86 @@
 package com.onramp.android.takehome.ui;
 
 
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+
+import com.onramp.android.takehome.R;
+import com.onramp.android.takehome.databinding.FragmentEventDetailBinding;
+import com.squareup.picasso.Picasso;
+
+import java.util.Objects;
+
+import timber.log.Timber;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class CharacterDetailFragment extends Fragment {
+public class EventFragment extends Fragment {
 
 
-    public CharacterDetailFragment() {
+    public EventFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        return textView;
+
+        Timber.d("FRAG ONCREATEVIEW starting ****");
+
+        FragmentEventDetailBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_event_detail, container, false);
+        View view =  binding.getRoot();
+        Bundle bundle = getArguments();
+
+        // Inflates views depending on what event was clicked in the EventsActivity
+        if (bundle != null) {
+            Timber.d("FRAG BUNDLE NOT NULL");
+            switch (Objects.requireNonNull(bundle.getString("eventKey"))) {
+                case "sd":
+                    Timber.d("FOUND SD KEY");
+                    binding.fragEventName.setText(getString(R.string.san_diego_comic_con));
+                    Picasso.get().load(getString(R.string.san_diego_imageUrl)).into(binding.fragEventImage);
+                    binding.fragEventDate.setText(getString(R.string.sd_event_date));
+                    binding.fragEventLocationTV.setText(getString(R.string.sd_event_location));
+                    binding.fragLearnMoreButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent eventDetailsIntent = new Intent(Intent.ACTION_VIEW);
+                            eventDetailsIntent.setData(Uri.parse(getString(R.string.comiccon_sanDiego_url)));
+                            startActivity(eventDetailsIntent);
+                        }
+                    });
+                    break;
+                case "ny":
+                    Timber.d("FOUND NY KEY");
+
+                    binding.fragEventName.setText(this.getString(R.string.new_york_comic_con));
+                    Picasso.get().load(this.getString(R.string.ny_imageUrl)).into(binding.fragEventImage);
+                    binding.fragEventDate.setText(this.getString(R.string.ny_event_date));
+                    binding.fragEventLocationTV.setText(this.getString(R.string.ny_event_location));
+                    binding.fragLearnMoreButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent eventDetailsIntent = new Intent(Intent.ACTION_VIEW);
+                            eventDetailsIntent.setData(Uri.parse(getString(R.string.comicon_ny_url)));
+                            startActivity(eventDetailsIntent);
+                        }
+                    });
+                    break;
+                default:
+                    Timber.d("*** Key not found ******");
+                    break;
+            }
+        }
+        return view;
+
+
     }
 
 }
--- app/src/main/java/com/onramp/android/takehome/ui/EventsActivity.java
+++ app/src/main/java/com/onramp/android/takehome/ui/EventsActivity.java
@@ -1,15 +1,63 @@
 package com.onramp.android.takehome.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.onramp.android.takehome.R;
+import com.squareup.picasso.Picasso;
+
+import timber.log.Timber;
 
 public class EventsActivity extends AppCompatActivity {
+
+    //    TextView sd_textView;
+//    TextView ny_textView;
+    ImageView sd_imageView;
+    ImageView ny_imageView;
+    Button sd_button;
+    Button ny_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events);
+
+        sd_imageView = findViewById(R.id.sand_diego_imageView);
+        ny_imageView = findViewById(R.id.ny_imageView);
+        sd_button = findViewById(R.id.sd_event_button);
+        ny_button = findViewById(R.id.ny_event_button);
+
+        Picasso.get().load(this.getString(R.string.san_diego_imageUrl)).into(sd_imageView);
+        Picasso.get().load(this.getString(R.string.ny_imageUrl)).into(ny_imageView);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        sd_button.setOnClickListener(v -> {
+            Timber.d("SD EVENT BUTTON CLICKED frag should launch");
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            EventFragment eventFragment = new EventFragment();
+            Bundle bundle = new Bundle();
+            bundle.putString("eventKey", "sd");
+            eventFragment.setArguments(bundle);
+            fragmentTransaction.add(R.id.events_activity, eventFragment)
+                    .addToBackStack(null);
+            fragmentTransaction.commit();
+        });
+
+        ny_button.setOnClickListener(v -> {
+            Timber.d("NY EVENT BUTTON CLICKED. Frag should launch");
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            EventFragment eventFragment = new EventFragment();
+            Bundle bundle = new Bundle();
+            bundle.putString("eventKey", "ny");
+            eventFragment.setArguments(bundle);
+            fragmentTransaction.add(R.id.events_activity, eventFragment)
+            .addToBackStack(null);
+            fragmentTransaction.commit();
+        });
     }
 }
