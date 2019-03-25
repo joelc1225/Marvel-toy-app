f396e28bbcfa8a777354635925f9c9aac747376d
Joel Camargo <joelc1225@gmail.com>
2019-03-25 10:57:23 -0400

    Create service for DB events, code the interaction between the Lottie animationView and the  db events from HeroOverviewActivity

--- app/src/main/java/com/onramp/android/takehome/DatabaseService.java
+++ app/src/main/java/com/onramp/android/takehome/DatabaseService.java
@@ -1,4 +1,71 @@
 package com.onramp.android.takehome;
 
-public class DatabaseService {
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+import com.onramp.android.takehome.data.HeroRepository;
+import com.onramp.android.takehome.model.Hero;
+
+import org.parceler.Parcels;
+
+import timber.log.Timber;
+
+public class DatabaseService extends IntentService {
+
+    public DatabaseService() {
+        super("DatabaseService");
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        Timber.d("SERVICE ON HANDLEINTENT CALLED");
+
+        String action = null;
+        Hero hero = null;
+
+        // Perform null checks before setting data and taking action
+        Bundle bundle = null;
+        if (intent != null) {
+            bundle = intent.getExtras();
+        }
+
+        if (bundle != null) {
+            Timber.d("Bundle NOT null. Extracting data from bundle");
+
+            if (bundle.containsKey("actionKey")) {
+                action = bundle.getString("actionKey");
+            } else {
+                Timber.d("NO ACTION KEY FOUND");
+            }
+
+            if (bundle.containsKey("heroKey")) {
+                hero = Parcels.unwrap(bundle.getParcelable("heroKey"));
+            } else {
+                Timber.d("NO HERO KEY FOUND");
+            }
+
+        } else {
+            Timber.d("Service Bundle is NULL");
+        }
+
+        // Perform database action based on data sent into the service
+        if (action != null && action.equals("insert")) {
+            Timber.d("ACTION is set to INSERT. Inserting now");
+            HeroRepository.getHeroDao().insert(hero);
+        }
+
+        if (action != null && action.equals("delete")) {
+            Timber.d("ACTION is set to DELETE. Deleting now");
+            HeroRepository.getHeroDao().delete(hero);
+        }
+    }
 }
--- app/src/main/java/com/onramp/android/takehome/ui/HeroOverviewActivity.java
+++ app/src/main/java/com/onramp/android/takehome/ui/HeroOverviewActivity.java
@@ -1,15 +1,20 @@
 package com.onramp.android.takehome.ui;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.onramp.android.takehome.R;
+import com.onramp.android.takehome.data.HeroRepository;
 import com.onramp.android.takehome.model.Hero;
+import com.onramp.android.takehome.utils.InjectorUtils;
 import com.squareup.picasso.Picasso;
 
 import org.parceler.Parcels;
@@ -22,6 +27,9 @@ public class HeroOverviewActivity extends AppCompatActivity {
     TextView descriptionTextView;
     ImageView heroImageView;
     Button detailsButton;
+    static LottieAnimationView lottieAnimationView;
+    static boolean mIsFavorite;
+    private static Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +40,10 @@ public class HeroOverviewActivity extends AppCompatActivity {
         descriptionTextView = findViewById(R.id.hero_description_TV);
         heroImageView = findViewById(R.id.hero_overview_imageview);
         detailsButton = findViewById(R.id.details_button);
+        lottieAnimationView = findViewById(R.id.lottie_fave_star);
+        mContext = this;
+        mIsFavorite = false;
+
 
         Bundle bundle = getIntent().getBundleExtra("bundle");
 
@@ -51,9 +63,60 @@ public class HeroOverviewActivity extends AppCompatActivity {
                         moreDetailsIntent.setData(Uri.parse(hero.detailsUrl));
                         startActivity(moreDetailsIntent);
                     });
+
+                    //Check if hero is already in the database. If so, adjust lottie animation to be colored in
+                    new querySingleHeroAsyncTask().execute(hero.name);
+
+                    // Clicking the Bookmark imageAnimation will Insert and Remove the Episode from the ROOM database
+                    lottieAnimationView.setOnClickListener(v -> {
+                        if (!mIsFavorite) {
+                            Timber.d("INSERTING FAVORITE");
+                            lottieAnimationView.setSpeed(1);
+                            lottieAnimationView.playAnimation();
+                            HeroRepository.insertFavoriteHero(hero, this);
+                            mIsFavorite = true;
+                        } else {
+                            Timber.d("DELETING FAVORITE");
+                            lottieAnimationView.setSpeed(-2f);
+                            lottieAnimationView.playAnimation();
+                            HeroRepository.deleteFavoriteHero(hero, this);
+                            mIsFavorite = false;
+                        }
+                    });
                 }
             } else {
                 Timber.d("NO HERO KEY");
+            }
+        }
+    }
+
+
+    private static class querySingleHeroAsyncTask extends AsyncTask<String, Void, Hero> {
+
+        // Constructor
+        querySingleHeroAsyncTask() {
+        }
+
+        @Override
+        protected Hero doInBackground(String... strings) {
+            Timber.d("INSIDE SINGLE HERO DIB");
+            return InjectorUtils.provideRepository(mContext).getSingleHeroById(strings[0]);
+        }
+
+        @Override
+        protected void onPostExecute(Hero hero) {
+            super.onPostExecute(hero);
+
+            if (hero == null) {
+                Timber.d("HERO NOT IN DATABASE");
+                // Do nothing
+            } else {
+                Timber.d("HERO ISSSSS IN DATABASE");
+                // Set animationView to be filled in and ready to reverse animation
+                lottieAnimationView.setProgress(1);
+                lottieAnimationView.setFrame(49);
+                lottieAnimationView.setSpeed(-2);
+                mIsFavorite = true;
             }
         }
     }
