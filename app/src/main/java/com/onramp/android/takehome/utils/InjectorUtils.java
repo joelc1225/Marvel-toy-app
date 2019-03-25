f9aa11d3eebfb553e8e42e64499478341af8be6f
Joel Camargo <joelc1225@gmail.com>
2019-03-25 10:51:53 -0400

    Create helper class that creates the repository class

--- app/src/main/java/com/onramp/android/takehome/utils/InjectorUtils.java
+++ app/src/main/java/com/onramp/android/takehome/utils/InjectorUtils.java
@@ -1,4 +1,20 @@
 package com.onramp.android.takehome.utils;
 
+import android.content.Context;
+
+import com.onramp.android.takehome.data.HeroRepository;
+import com.onramp.android.takehome.room.HeroDatabase;
+
 public class InjectorUtils {
+
+    /**
+     * Provides static method to inject various classes needed for app
+     * one instantiation per app run through
+     */
+
+    public static HeroRepository provideRepository(Context context) {
+
+        HeroDatabase heroDatabase = HeroDatabase.getInstance(context.getApplicationContext());
+        return HeroRepository.getInstance(heroDatabase.heroDao());
+    }
 }
